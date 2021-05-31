61d2ccca220e39e14a1a91bd781276f1d808d662
Peter Bloxidge <peter.bloxidge@virgin.net>
2021-05-31 22:50:47 +0100

    Add default compositional layout for collection view

--- GitSearch/Modules/RepoList/View/RepoListViewController.swift
+++ GitSearch/Modules/RepoList/View/RepoListViewController.swift
@@ -7,10 +7,30 @@
 
 import UIKit
 
-class RepoListViewController: UIViewController {
+class RepoListViewController: UICollectionViewController {
     
     var presenter: RepoListPresenter!
+    
+    convenience init() {
+        self.init(collectionViewLayout: Self.createCompositionalLayout())
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Repositories"
+        
+        setUpCollectionView()
+        
+        presenter.attachToView()
+    }
+    
+    private func setUpCollectionView() {
+        collectionView.backgroundColor = .systemBackground
+    }
 }
+
+// MARK: - RepoListView
 
 extension RepoListViewController: RepoListView {
     
@@ -20,5 +40,35 @@ extension RepoListViewController: RepoListView {
             // Set up initial view state
             break
         }
+    }
+}
+
+// MARK: - UICollectionViewCompositionalLayout
+
+extension RepoListViewController {
+    
+    static func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(80)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitem: item,
+            count: 1
+        )
+        let spacing: CGFloat = 8
+        group.interItemSpacing = .fixed(spacing)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = spacing
+        
+        return UICollectionViewCompositionalLayout(section: section)
     }
 }
