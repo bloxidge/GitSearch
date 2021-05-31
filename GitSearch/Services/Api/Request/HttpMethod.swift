45f1774966343485a37312fc012a5e7bbd155e61
Peter Bloxidge <peter.bloxidge@virgin.net>
2021-05-31 18:54:27 +0100

    Add generic API service

---
+++ GitSearch/Services/Api/Request/HttpMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HttpMethod.swift
+//  GitSearch
+//
+//  Created by Peter Bloxidge on 31/05/2021.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case get
+    case post
+    case put
+    case delete
+}
