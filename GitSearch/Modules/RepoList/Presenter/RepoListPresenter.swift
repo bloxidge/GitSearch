7731fbccb19870763aac10778e91d80c0a4fb953
Peter Bloxidge <peter.bloxidge@virgin.net>
2021-06-01 19:11:46 +0100

    Add presenter proxy methods to interactor

--- GitSearch/Modules/RepoList/Presenter/RepoListPresenter.swift
+++ GitSearch/Modules/RepoList/Presenter/RepoListPresenter.swift
@@ -10,6 +10,10 @@ import PromiseKit
 
 protocol RepoListPresenter {
     func attachToView()
+    @discardableResult
+    func performSearch(_ searchQuery: String) -> Promise<Void>
+    func getVisibleCount() -> Int?
+    func getRepository(at index: Int) -> Repository?
 }
 
 class RepoListPresenterImpl: RepoListPresenter {
@@ -20,5 +24,33 @@ class RepoListPresenterImpl: RepoListPresenter {
     
     func attachToView() {
         view.updateView(state: .initial)
+    }
+    
+    @discardableResult
+    func performSearch(_ searchQuery: String) -> Promise<Void> {
+        view.updateView(state: .loading)
+        
+        let promise = interactor.fetchRepoSearchResults(searchQuery)
+        promise
+            .done { results in
+                if results.items.isEmpty {
+                    self.view.updateView(state: .doneEmpty)
+                } else {
+                    self.view.updateView(state: .doneResults)
+                }
+            }
+            .catch { error in
+                print(error)
+                self.view.updateView(state: .error)
+            }
+        return promise.asVoid()
+    }
+    
+    func getVisibleCount() -> Int? {
+        interactor.results?.items.count
+    }
+    
+    func getRepository(at index: Int) -> Repository? {
+        interactor.results?.items[index]
     }
 }
