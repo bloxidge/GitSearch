a7b5d52bc185a4449b39314a383a43b7c1cce85b
Peter Bloxidge <peter.bloxidge@virgin.net>
2021-06-05 20:52:24 +0100

    Install SwiftyMocky for auto-mocking dependencies

---
+++ GitSearchTests/Mocks/Mock.generated.swift
@@ -0,0 +1,16 @@
+// Generated using Sourcery 1.0.2 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+
+// Generated with SwiftyMocky 4.0.1
+
+import SwiftyMocky
+import XCTest
+import Foundation
+import PromiseKit
+@testable import GitSearch
+
+
+// SwiftyMocky: no AutoMockable found.
+// Please define and inherit from AutoMockable, or annotate protocols to be mocked
